ed30a4253e3cebeff539bb0e56f2daea2139da4a
hammadalive7 <200862@students.au.edu.pk>
2023-07-18 00:15:10 +0500

    initial commit

---
+++ lib/Home_Screen.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:restapi_flutter/Model/PostModel.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postList = [];
+
+  Future<List<PostModel>> getPostApi() async {
+    final responce =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    var data = jsonDecode(responce.body.toString());
+    if (responce.statusCode == 200) {
+      postList.clear();
+      for (Map i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Screen',
+              style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold)),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                future: getPostApi(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else {
+                    return ListView.builder(
+                      itemCount: postList.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              elevation: 5,
+                              borderOnForeground: true,
+                              color: Colors.grey[200],
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              shadowColor: Colors.deepPurple,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              surfaceTintColor: Colors.deepPurple,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      "Title",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      postList[index].title.toString(),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    const Text(
+                                      "Body",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      postList[index].body.toString(),
+                                      style: const TextStyle(
+                                        overflow: TextOverflow.ellipsis,
+                                        wordSpacing: 1.5,
+
+                                      ),
+
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ));
+                      },
+                    );
+                  }
+                },
+              ),
+            )
+          ],
+        ));
+  }
+}
